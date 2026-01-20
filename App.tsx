cd1c4af460b8c56f0aa67ceda0321e7aa70598ce
Kelwin Tobias <kelwintobiascomercial@gmail.com>
2026-01-20 13:08:03 -0300

    fix: enable VistraLanding component to display High Ticket changes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-// import { VistraLanding } from './components/VistraLanding';
-import { VistraConcierge } from './components/VistraConcierge';
+import { VistraLanding } from './components/VistraLanding';
+// import { VistraConcierge } from './components/VistraConcierge';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-black flex flex-col">
-      {/* <VistraLanding /> */}
-      <VistraConcierge />
+      <VistraLanding />
+      {/* <VistraConcierge /> */}
     </div>
   );
 };
